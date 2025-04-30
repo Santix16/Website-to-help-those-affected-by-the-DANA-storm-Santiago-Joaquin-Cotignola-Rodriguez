cbc1703b08263b1038670b311bda741ab560b2c4
Santix16 <162974917+Santix16@users.noreply.github.com>
2025-04-30 23:48:03 +0200

    Update perfil_privado.php

--- TutelaFinalVersion/users/perfil_privado.php
+++ TutelaFinalVersion/users/perfil_privado.php
@@ -47,17 +47,9 @@ if (!isset($usuario['valoracion']) || $usuario['valoracion'] < 1 || $usuario['va
     <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 <body class="landing">
-    <header id="header" class="alt">
-        <h1><strong><a href="../index.php">TELE-DANA</a></strong> - Perfil Privado</h1>
-        <nav id="nav">
-            <ul>
-                <li><a href="../index.php">Inicio</a></li>
-                <li><a href="../tienda.php">Tienda</a></li>
-                <li><a href="perfil_privado.php">Perfil</a></li>
-                <li><a href="../logout.php">Cerrar Sesión</a></li>
-            </ul>
-        </nav>
-    </header>
+
+    <!-- Aquí se incluye el header desde el archivo includes/header.php -->
+    <?php include('../includes/header.php'); ?>
 
     <section id="banner">
         <h2>Perfil del Usuario</h2>
@@ -142,4 +134,5 @@ if (!isset($usuario['valoracion']) || $usuario['valoracion'] < 1 || $usuario['va
     </div>
 </body>
 </html>
+
 
