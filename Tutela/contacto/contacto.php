6555815e8aa2ed859eab3c9806b8b6202e82dd8d
LJunLL <ljunll0000@gmail.com>
2025-04-30 19:56:47 +0200

    Update contacto.php

--- Tutela/contacto/contacto.php
+++ Tutela/contacto/contacto.php
@@ -7,20 +7,7 @@
     <link rel="stylesheet" href="../assets/css/main.css" />
 </head>
 <body class="landing">
-
-   <!-- Header -->
-<header id="header" class="alt">
-  <h1><strong><a href="index.html">Tutela La DANA</a></strong></h1>
-  <nav id="nav">
-    <ul>
-      <li><a href="../index.php">Inicio</a></li>
-      <li><a href="../servicios.php">Servicios</a></li>
-      <li><a href="../quienes_somos.html">Quiénes Somos</a></li>
-      <li><a href="../contacto/contacto.php">Contacto</a></li>
-      <li><a href="../login.html">Login</a></li>
-    </ul>
-  </nav>
-</header>
+    <?php include("includes/header.php"); ?>
     <a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
 
     <!-- Banner -->
