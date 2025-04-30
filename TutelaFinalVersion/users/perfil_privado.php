08c0661669521aac9ecee51cd3b135ccb346e492
Santix16 <162974917+Santix16@users.noreply.github.com>
2025-04-30 23:44:02 +0200

    Update perfil_privado.php

--- TutelaFinalVersion/users/perfil_privado.php
+++ TutelaFinalVersion/users/perfil_privado.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include '../includes/db.php';
 // Si el usuario NO ha iniciado sesión, mostramos un mensaje o redirigimos
 if (!isset($_SESSION['usuario'])) {
     echo '
@@ -47,17 +47,6 @@ if (!isset($usuario['valoracion']) || $usuario['valoracion'] < 1 || $usuario['va
     <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 <body class="landing">
-    <header id="header" class="alt">
-        <h1><strong><a href="../index.php">TELE-DANA</a></strong> - Perfil Privado</h1>
-        <nav id="nav">
-            <ul>
-                <li><a href="../index.php">Inicio</a></li>
-                <li><a href="../tienda.php">Tienda</a></li>
-                <li><a href="perfil_privado.php">Perfil</a></li>
-                <li><a href="../logout.php">Cerrar Sesión</a></li>
-            </ul>
-        </nav>
-    </header>
 
     <section id="banner">
         <h2>Perfil del Usuario</h2>
