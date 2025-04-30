0dd8b26470636a27ac191c9043eccc789db9dfcb
LJunLL <ljunll0000@gmail.com>
2025-04-30 17:28:25 +0200

    Update index.php

--- Tutela/index.php
+++ Tutela/index.php
@@ -30,23 +30,7 @@
 
 
 <!-- Footer -->
-<footer id="footer" style="
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    padding: 0.5em 0;
-    background: #f5f5f5;
-    text-align: center;
-    font-size: 0.8em;
-    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);">
-  <div class="container">
-    <ul class="copyright" style="margin: 0; padding: 0; list-style: none;">
-      <li style="display: inline;">© 2025 TELE-DANA. Todos los derechos reservados.</li>
-      <li style="display: inline; margin-left: 10px;"><a href="legal/legal.html">Términos de Uso</a></li>
-      <li style="display: inline; margin-left: 10px;"><a href="legal/privacidad.html">Protección de Datos</a></li>
-    </ul>
-  </div>
-</footer>
+<?php include("includes/footer.php"); ?>
 
 <!-- Scripts -->
 <script src="assets/js/jquery.min.js"></script>
